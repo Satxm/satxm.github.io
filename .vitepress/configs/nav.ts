d74b4a7ce307d1bcca9745058924f4485c354e36
Satxm <satxm@outlook.com>
2025-03-08 10:39:38 +0800

    Add top nav

--- .vitepress/configs/nav.ts
+++ .vitepress/configs/nav.ts
@@ -2,4 +2,8 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
     { text: '首页', link: '/' },
+    { text: 'Linux', link: '/linux/' },
+    { text: 'Windows', link: '/windows/' },
+    { text: 'Docker', link: '/docker' },
+    { text: 'Software', link: '/software' },
 ]
