3eef2b14bd1d8a4f089aab1eb2d974fc240caef3
Satxm <satxm@outlook.com>
2025-03-08 17:12:12 +0800

    Update hitokoto

--- .vitepress/theme/Layout/index.vue
+++ .vitepress/theme/Layout/index.vue
@@ -7,6 +7,14 @@ import { toggleAppearance } from './toggleAppearance'
 import { hitokoto } from './hitokoto'
 toggleAppearance(isDark) //实现切换主题过渡动画
 hitokoto() //一言 Hitokoto
+document.addEventListener('click', function (event) {
+  if ((<HTMLElement>event.target).innerText == '首页') {
+    hitokoto()
+  }
+  if ((<HTMLElement>event.target).innerText == 'Satxm\'s Book') {
+    hitokoto()
+  }
+})
 </script>
 
 <template>
